6be980efa16764e3615d9026035075bc2cadc17b
Doramon <verx_2544@hotmail.com>
2022-07-01 17:19:52 +0700

    Day2

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,3 +1,6 @@
+import 'package:boneclinicmsu/unility/my_constant.dart';
+import 'package:boneclinicmsu/widgets/show_image.dart';
+import 'package:boneclinicmsu/widgets/show_title.dart';
 import 'package:flutter/material.dart';
 
 class Authen extends StatefulWidget {
@@ -10,6 +13,40 @@ class Authen extends StatefulWidget {
 class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            builImage(size),
+            buildAppName(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(
+          title: MyConstant.appName,
+          textStyle: MyConstant().h1style(),
+        ),
+      ],
+    );
+  }
+
+  Row builImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.8,
+          child: ShowImage(path: MyConstant.image5),
+        ),
+      ],
+    );
   }
 }
--- lib/unility/my_constant.dart
+++ lib/unility/my_constant.dart
@@ -10,4 +10,36 @@ class MyConstant {
   static String routeCoustomerService = 'customerService';
   static String routeDoctorService = 'doctorService';
   static String routeAdminService = 'adminService';
+
+  // Image
+  static String image1 = 'images/image (1).png';
+  static String image2 = 'images/image (2).png';
+  static String image3 = 'images/image (3).png';
+  static String image4 = 'images/image (4).png';
+  static String image5 = 'images/image (5).png';
+  static String image6 = 'images/image (6).png';
+  static String image7 = 'images/image (7).png';
+  static String image8 = 'images/image (8).png';
+
+  //Color
+  static Color primary = Color(0xffef9a9a);
+  static Color dark = Color(0xffba6b6c);
+  static Color light = Color(0xffffcccb);
+
+  // Style
+  TextStyle h1style() => TextStyle(
+        fontSize: 24,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle h2style() => TextStyle(
+        fontSize: 18,
+        color: dark,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle h3style() => TextStyle(
+        fontSize: 14,
+        color: dark,
+        fontWeight: FontWeight.normal,
+      );
 }
